4d569d197d7534c31ac3f1a13c7e6b4594c2da3c
Alexes Mes <akm73@cam.ac.uk>
2023-07-18 22:59:31 +0100

    GPQR Model on actual Bantu Dates
    Note: figures are placeholder figures to check if code works. Number of MCMC iterations is very low at this stage: n=20

--- bantu_migration/scripts/figures.R
+++ bantu_migration/scripts/figures.R
@@ -27,6 +27,7 @@ source(here('src','gpqrSim.R'))
 
 # Load Observed Data
 load(here('data','c14.RData'))
+
 # Load quantile regression results
 load(here('output','quantreg_res.RData'))
 
@@ -273,8 +274,9 @@ dev.off()
 #--------------
 # Tactical Simulation ---- FIGURE 8
 
+# Load data
 load(here('data', 'tactical_sim_gpqr.RData'))
-load(here('output', 'gpqr_tactsim.RData')) ##TODO: rerun this once tactical data has been generated
+load(here('output', 'gpqr_tactsim.RData'))
 
 gpqr_tactsim_post  <- do.call(rbind, gpqr_tactsim)
 tactsim_post_s  <- gpqr_tactsim_post[ ,paste0('s[',1:nrow(sim_sites),']')]
@@ -359,14 +361,187 @@ dev.off()
 
 
 #===============================================================================
-## qpqr_tau90.R Figures ----
+## qpqr_tau90.R and qpqr_tau99.R Figures ----
+
+# Load data
+load(here('output','gpqr_tau90.RData'))
+#load(here('output','gpqr_tau99.RData'))
+
+# Obtain Background  Map
+win  <- ne_countries(continent = 'africa', scale=10, returnclass='sf')
+
+#-------------------------------------------------------------------------------
+# Posterior Mean of dispersal rate deviations ---- FIGURE 11
+
+#TODO: rerun with gpqr_tau90.RData and gpqr_tau99.RData once generated!
+post.gpqr.tau90  <- do.call(rbind,gpqr_tau90)
+#post.gpqr.tau99  <- do.call(rbind,gpqr_tau99)
+
+nmcmc  <- nrow(post.gpqr.tau90) #number of MCMC samples (same for tau90 and tau99)
+post.s.tau90  <- post.gpqr.tau90[,grep('s\\[',colnames(post.gpqr.tau90))]
+#post.s.tau99  <- post.gpqr.tau99[,grep('s\\[',colnames(post.gpqr.tau99))]
+
+# post.arrival <- matrix(NA,nmcmc,constants$N.sites)
+#post.rate.tau90 <- post.rate.tau99  <-  matrix(NA,nmcmc,constants$n_sites)
+post.rate.tau90 <-  matrix(NA, nmcmc, constants$n_sites)
+for (i in 1:nmcmc)
+{
+  post.rate.tau90[i,] = -1 / (post.s.tau90[i,]-post.gpqr.tau90[i,'beta1'])
+  #post.rate.tau99[i,] = -1 / (post.s.tau99[i,]-post.gpqr.tau99[i,'beta1'])
+}
+
+sites@data$s.m.tau90 <- apply(post.s.tau90,2,median)
+#sites@data$s.m.tau99 <- apply(post.s.tau99,2,median)
+sites@data$rate.m.tau90  <- apply(post.rate.tau90,2,median)
+#sites@data$rate.m.tau99  <- apply(post.rate.tau99,2,median)
+
+sites.sf <- as(sites,'sf')
+
+f2a <- ggplot() +
+  geom_text(data=data.frame(x=49, y=32, label='A'), aes(x=x, y=y, label=label)) +
+  geom_sf(data=win, aes(), fill='grey66', show.legend=FALSE, lwd=0) +
+  geom_sf(data=sites.sf, mapping = aes(fill=s.m.tau90), pch=21, col='black', size=2) + 
+  xlim(-15,50) + #Center the frame on sub-Saharan Africa
+  ylim(-35,35) +
+  labs(title=TeX(r"(Posterior Median of s with $\tau = 0.90$)"), fill='s') + 
+  scale_fill_gradient2(low='blue', high='red', mid='white') +
+  theme(plot.title = element_text(hjust = 0.5, vjust = -1.5, size=10), 
+        panel.background = element_rect(fill='lightblue'), 
+        panel.grid.major = element_line(size = 0.1), 
+        legend.position=c(0.2,0.2), 
+        legend.text = element_text(size=6), 
+        legend.key.width= unit(0.1, 'in'), 
+        legend.key.size = unit(0.08, "in"), 
+        legend.background=element_rect(fill = alpha("white", 0.5)), 
+        legend.title=element_text(size=7), 
+        axis.text=element_blank(), 
+        axis.ticks=element_blank(), 
+        plot.margin = unit(c(0,0,0,0), "in"), 
+        axis.title.x = element_blank(), 
+        axis.title.y = element_blank())
+
+# f2b <- ggplot() +
+#   geom_text(data=data.frame(x=49, y=32, label='B'), aes(x=x, y=y, label=label)) +
+#   geom_sf(data=win, aes(), fill='grey66', show.legend=FALSE, lwd=0) +
+#   geom_sf(data=sites.sf, mapping = aes(fill=s.m.tau99), pch=21, col='black', size=2) + 
+#   xlim(-15,50) + #Center the frame on sub-Saharan Africa
+#   ylim(-35,35) +
+#   labs(title=TeX(r"(Posterior Median of s with $\tau = 0.99$)"), fill='s') + 
+#   scale_fill_gradient2(low='blue', high='red', mid='white') +
+#   theme(plot.title = element_text(hjust = 0.5, vjust = -1.5, size=10), 
+#         panel.background = element_rect(fill='lightblue'), 
+#         panel.grid.major = element_line(size = 0.1), 
+#         legend.position=c(0.2,0.2), 
+#         legend.text = element_text(size=6), 
+#         legend.key.width= unit(0.1, 'in'), 
+#         legend.key.size = unit(0.08, "in"), 
+#         legend.background=element_rect(fill = alpha("white", 0.5)), 
+#         legend.title=element_text(size=7), 
+#         axis.text=element_blank(), 
+#         axis.ticks=element_blank(), 
+#         plot.margin = unit(c(0,0,0,0), "in"), 
+#         axis.title.x = element_blank(), 
+#         axis.title.y = element_blank())
+
+f2c <- ggplot() +
+  geom_text(data=data.frame(x=49, y=32, label='C'), aes(x=x, y=y, label=label)) +
+  geom_sf(data=win, aes(), fill='grey66', show.legend=FALSE, lwd=0) +
+  geom_sf(data=sites.sf, mapping = aes(fill=rate.m.tau90), pch=21, col='black', size=2) + 
+  xlim(-15,50) + #Center the frame on sub-Saharan Africa
+  ylim(-35,35) +
+  labs(title=TeX(r"(Posterior median of dispersal rate with $\tau = 0.90$)"), fill='Dispersal Rate (km/year)') +
+  scale_fill_viridis(option="turbo", limits=c(0,4)) +
+  theme(plot.title = element_text(hjust = 0.5, vjust = -1.5, size=10), 
+        panel.background = element_rect(fill='lightblue'), 
+        panel.grid.major = element_line(size = 0.1), 
+        legend.position=c(0.21,0.2), 
+        legend.text = element_text(size=6), 
+        legend.key.width= unit(0.1, 'in'), 
+        legend.key.size = unit(0.08, "in"), 
+        legend.background=element_rect(fill = alpha("white", 0.5)), 
+        legend.title=element_text(size=7), 
+        axis.text=element_blank(), 
+        axis.ticks=element_blank(), 
+        plot.margin = unit(c(0,0,0,0), "in"), 
+        axis.title.x = element_blank(), 
+        axis.title.y = element_blank())
+
+# f2d <- ggplot() +
+#   geom_text(data=data.frame(x=49, y=32, label='D'), aes(x=x, y=y, label=label)) +
+#   geom_sf(data=win, aes(), fill='grey66', show.legend=FALSE, lwd=0) +
+#   geom_sf(data=sites.sf, mapping = aes(fill=rate.m.tau99), pch=21, col='black', size=2) + 
+#   xlim(-15,50) + #Center the frame on sub-Saharan Africa
+#   ylim(-35,35) +
+#   labs(title=TeX(r"(Posterior median of dispersal rate with $\tau = 0.99$)"), fill='Dispersal Rate (km/year)') +
+#   scale_fill_viridis(option="turbo", limits=c(0,4)) +
+#   theme(plot.title = element_text(hjust = 0.5, vjust = -1.5, size=10), 
+#         panel.background = element_rect(fill='lightblue'), 
+#         panel.grid.major = element_line(size = 0.1), 
+#         legend.position=c(0.21,0.2), 
+#         legend.text = element_text(size=6), 
+#         legend.key.width= unit(0.1, 'in'), 
+#         legend.key.size = unit(0.08, "in"), 
+#         legend.background=element_rect(fill = alpha("white", 0.5)), 
+#         legend.title=element_text(size=7), 
+#         axis.text=element_blank(), 
+#         axis.ticks=element_blank(), 
+#         plot.margin = unit(c(0,0,0,0), "in"), 
+#         axis.title.x = element_blank(), 
+#         axis.title.y = element_blank())
 
 
+pdf(file=here('output','figures','figure11.pdf'), width=7, height=7)
+grid.arrange(f2a, f2a, f2c, f2c, ncol=2, padding=0)
+#grid.arrange(f2a, f2b, f2c, f2d, ncol=2, padding=0)
+dev.off()
 
+#-------------------------------------------------------------------------------
+# Traceplot of beta0, beta1, rhosq, and etasq for tau=0.9) ---- FIGURE 12
 
+pdf(file=here('output','figures','figure12.pdf'), width=8, height=8)
+par(mfrow=c(2,2))
+traceplot(gpqr_tau90[,'beta0'], main=TeX('$\\beta_0$'), smooth=TRUE)
+traceplot(gpqr_tau90[,'beta1'], main=TeX('$\\beta_1$'), smooth=TRUE)
+traceplot(gpqr_tau90[,'rho'], main=TeX('$\\rho$'), smooth=TRUE)
+traceplot(gpqr_tau90[,'etasq'], main=TeX('$\\eta^2$'), smooth=TRUE)
+dev.off()
 
+#-------------------------------------------------------------------------------
+# Traceplot of beta0, beta1, rhosq, and etasq for tau=0.99 ---- FIGURE 13
 
+# pdf(file=here('output','figures','figure13.pdf'), width=8, height=8)
+# par(mfrow=c(2,2))
+# traceplot(gpqr_tau99[,'beta0'], main=TeX('$\\beta_0$'), smooth=TRUE)
+# traceplot(gpqr_tau99[,'beta1'], main=TeX('$\\beta_1$'), smooth=TRUE)
+# traceplot(gpqr_tau99[,'rho'], main=TeX('$\\rho$'), smooth=TRUE)
+# traceplot(gpqr_tau99[,'etasq'], main=TeX('$\\eta^2$'), smooth=TRUE)
+# dev.off()
 
+#-------------------------------------------------------------------------------
+# Marginal posteriors of beta0, beta1, rho, etasq for tau = 0.9 ---- FIGURE 14
+
+gpqr.tau90.comb  <- do.call(rbind, gpqr_tau90)
+
+pdf(file=here('output','figures','figure14.pdf'), width=8, height=8)
+par(mfrow=c(2,2))
+postHPDplot(gpqr.tau90.comb[,'beta0'], main=TeX('$\\beta_0$'), xlab='Cal BP', ylab='')
+postHPDplot(gpqr.tau90.comb[,'beta1'], main=TeX('$\\beta_1$'), xlab='', ylab='')
+postHPDplot(gpqr.tau90.comb[,'rho'], main=TeX('$\\rho$'), xlab='km', ylab='')
+postHPDplot(gpqr.tau90.comb[,'etasq'], main=TeX('$\\eta^2$'), xlab='', ylab='')
+dev.off()
+
+#-------------------------------------------------------------------------------
+# Marginal posteriors of beta0, beta1, rho, etasq for tau = 0.99 ---- FIGURE 15
+
+gpqr.tau99.comb  <- do.call(rbind, gpqr_tau99)
+
+# pdf(file=here('output','figures','figure15.pdf'), width=8, height=8)
+# par(mfrow=c(2,2))
+# postHPDplot(gpqr.tau99.comb[,'beta0'], main=TeX('$\\beta_0$'), xlab='Cal BP', ylab='')
+# postHPDplot(gpqr.tau99.comb[,'beta1'], main=TeX('$\\beta_1$'), xlab='', ylab='')
+# postHPDplot(gpqr.tau99.comb[,'rho'], main=TeX('$\\rho$'), xlab='km', ylab='')
+# postHPDplot(gpqr.tau99.comb[,'etasq'], main=TeX('$\\eta^2$'), xlab='', ylab='')
+# dev.off()
 
 
 #===============================================================================
