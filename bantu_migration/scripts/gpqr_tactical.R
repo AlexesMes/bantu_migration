6158e0c214ea2443e404945d0cc5504b448d4539
Alexes Mes <akm73@cam.ac.uk>
2023-07-27 21:20:44 +0100

    GPQR Tactical Simulation with n=20000 iterations
    Model failing to recover true parameter values

--- bantu_migration/scripts/gpqr_tactical.R
+++ bantu_migration/scripts/gpqr_tactical.R
@@ -132,8 +132,8 @@ ncores <- 4
 cl <- makeCluster(ncores)
 # Run the model in parallel:
 seeds <- c(12,45,67,89)
-niter = 1000000
-nburnin = 500000
+niter = 20000
+nburnin = 10000
 thin = 50
 chain_output <- parLapply(cl = cl, X = seeds, fun = runFun, dat = dat, constants = constants, theta = theta_init, niter = niter, nburnin = nburnin,thin = thin)
 stopCluster(cl)
