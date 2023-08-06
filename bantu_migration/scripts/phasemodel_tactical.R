4dda980646ebdd0c17ae14c9888efadf05abacdd
Alexes Mes <akm73@cam.ac.uk>
2023-08-06 15:28:55 +0100

    Clean up phase model simulation

--- bantu_migration/scripts/phasemodel_tactical.R
+++ bantu_migration/scripts/phasemodel_tactical.R
@@ -35,13 +35,13 @@ constants1 <- list(Ndates = sim_constants$Ndates,
 
 #Initialise parameters ---- 
 d1 <- list(cra=sim_df$cra, cra_error=sim_df$cra_error, unif.const=1)
-theta.init = medCal(calibrate(d1$cra, 
+theta_init = medCal(calibrate(d1$cra, 
                               d1$cra_error, 
                               verbose = FALSE))
 
 inits1 <- list(a=5000,
                b=500,
-               theta=theta.init)
+               theta=theta_init)
 
 
 #Run MCMC ----
@@ -73,7 +73,7 @@ model2 <- nimbleCode({
   }
   
   for (i in 1:Ndates){
-    theta[i] ~ dunif(min=(alpha[id_sites[i]] - (delta[id_sites[i]]+1)), max=alpha[id_sites[i]]); 
+    theta[i] ~ dunif(min = (alpha[id_sites[i]] - (delta[id_sites[i]]+1)), max = alpha[id_sites[i]]); 
     #Calibration
     mu[i] <- interpLin(z=theta[i], x=calBP[], y=C14BP[]);
     sigmaCurve[i] <- interpLin(z=theta[i], x=calBP[], y=C14err[]);
@@ -100,22 +100,22 @@ constants2 <- list(Ndates=sim_constants$Ndates,
 
 #Initialise parameters ----
 d2 <- list(cra=sim_df$cra, cra_error=sim_df$cra_error, unif.const=1)
-theta.init  <-  medCal(calibrate(d1$cra,
-                                 d1$cra_error,
+theta_init  <-  medCal(calibrate(d2$cra,
+                                 d2$cra_error,
                                  verbose = FALSE))
-dd  <-  data.frame(theta=theta.init, id=constants2$id_sites)
+dd  <-  data.frame(theta=theta_init, id=constants2$id_sites)
 buffer  <- 100
 earliest  <- aggregate(theta~id, max, data=dd) #Earliest date at each site
 latest  <- aggregate(theta~id, min, data=dd) #Latest date at each site
-diff.age  <- earliest$theta - latest$theta
-delta.init  <- diff.age + buffer
-alpha.init  <- earliest$theta + buffer/2
+diff_age  <- earliest$theta - latest$theta
+delta_init  <- diff_age + buffer
+alpha_init  <- earliest$theta + buffer/2
 
 inits2 <- list(a = 5000,
                b = 500,
-               theta = theta.init,
-               alpha = alpha.init,
-               delta = delta.init,
+               theta = theta_init,
+               alpha = alpha_init,
+               delta = delta_init,
                gamma1 = 5,
                gamma2 = 200)
 
