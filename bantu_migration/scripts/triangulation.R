b0b26159629037261f65bc1de79c51a1e4f3d706
Alexes Mes <akm73@cam.ac.uk>
2024-01-17 11:30:00 +0000

    Df of distance between transitions

--- bantu_migration/scripts/triangulation.R
+++ bantu_migration/scripts/triangulation.R
@@ -36,22 +36,27 @@ bantu_sites_sf <- sf::st_as_sf(siteInfo,
 ## Compute Great-Arc Distances in km between area centers ---
 #We take the center of each area k to be a point representing that whole area
 hex_area_centers <- hex_area_win$area_center %>% as('Spatial')
+proj4string(hex_area_centers)  <- "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"
 hex_dist_mat <- spDists(hex_area_centers, longlat=TRUE) #Inter-area distance matrix: each area's distance from every other area.
 
 
-#Delaunay triangulation between hex centers
-del <- deldir(hex_area_centers@coords, id=hex_area_win$area_ID)
+##Delaunay triangulation between hex centers
+#center_coords <- hex_area_centers@coords #TODO: Uncomment
+#del <- deldir(center_coords, id=hex_area_win$area_ID) 
+#tiles <- tile.list(del)
+##Selecting 3 triangles of interest -- TEST CASE
+center_coords <- hex_area_centers@coords[c(13, 14, 18), ]
+del <- deldir(center_coords, id=c(13,14, 18))
 tiles <- tile.list(del)
 
 
-
 ##Plot delaunay triangulation
-ggplot(data = hex_area_win) +
+ggplot(data = hex_area_win[c(13,14, 18),]) + #ggplot(data = hex_area_win) + #TODO: Uncomment
   geom_sf(data = st_buffer(as(gUnaryUnion(sampling_win), 'sf'), 40000), aes(color = "grey50")) + #sampling window with coastal buffer
   geom_sf() + #hex grid
-  #geom_sf_text(aes(label = area_ID), size=2, alpha=0.5) + #hex grid labels
+  geom_sf_text(aes(label = area_ID), size=4, alpha=0.8) + #hex grid labels
   geom_sf(data = hex_area_win$area_center, size=2, alpha=1, aes(color = "purple")) + #hex-origins
-  geom_delaunay_segment(aes(x=hex_area_centers@coords[,1], y=hex_area_centers@coords[,2]), 
+  geom_delaunay_segment(aes(x=center_coords[,1], y=center_coords[,2]), 
                         alpha=0.5, 
                         colour='purple',
                         size=0.8) +
@@ -62,6 +67,26 @@ ggplot(data = hex_area_win) +
                                         linetype = "solid"),
         legend.position = "none")
 
+#-------------------------------------------------------------------------------
+##Transitions dataframe --
+transitions <- del$delsgs %>% 
+  dplyr::select(region1_id = ind1, 
+                region2_id = ind2,
+                region1_x = x1,
+                region1_y = y1,
+                region2_x = x2,
+                region2_y = y1) %>% 
+  mutate(region1_id = as.integer(region1_id), 
+         region2_id = as.integer(region2_id)) %>% 
+  rowwise() %>% 
+  mutate(distance = hex_dist_mat[region1_id, region2_id]) #Great-arc distance between transitions in km
 
+#-------------------------------------------------------------------------------
+## Save everything on a R image file ----
+save(del, 
+     tiles, 
+     transitions,
+     center_coords, 
+     file=here('data','trig.RData'))
 
 
